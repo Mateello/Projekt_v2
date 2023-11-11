46dc8539cb0f6041f512897093764778617109a2
Matelusz <you@example.com>
2023-11-11 11:47:14 +0100

    Pierwsza wersja okienek i wrogów

--- Projekt_v2.cpp
+++ Projekt_v2.cpp
@@ -1,14 +1,19 @@
 ﻿#include <SFML/Graphics.hpp>
 int main()
 {
+	//OKNA - DOCELOWO ZROBIĆ Z TEGO KLASE
 	sf::RenderWindow menu(sf::VideoMode(600.f, 600.f), "Menu");
 	sf::RenderWindow glowne(sf::VideoMode(400.f, 400.f), "Projekt-PACMAN");
 	sf::RenderWindow help(sf::VideoMode(200.f, 200.f), "Pomoc");
-	sf::RenderWindow Esc(sf::VideoMode(200.f, 200.f), "Opuszczanie gry");
+	sf::RenderWindow esc(sf::VideoMode(200.f, 200.f), "Opuszczanie gry");
 
+	glowne.close();//trzeba uważać bo te okna i tak się na chwile pokażą
+	help.close();
+	esc.close();
+
+	//WROGOWIE - DOCELOWO ZROBIĆ Z TEGO KLASE
 	sf::CircleShape wrog_1(100.f);
 	sf::CircleShape wrog_2(100.f, 3);
-
 	sf::RectangleShape wrog_3(sf::Vector2f(150.f, 100.f));
 	sf::ConvexShape wrog_4;
 	wrog_4.setPointCount(5);
@@ -18,17 +23,24 @@ int main()
 	wrog_4.setPoint(3, sf::Vector2f(20.f, 50.f));
 	wrog_4.setPoint(4, sf::Vector2f(0.f, 70.f));
 
-	while (glowne.isOpen())
+	sf::Color wr_kl (sf::Color::Red);//kolor wrogów
+	wrog_1.setFillColor(wr_kl); wrog_2.setFillColor(wr_kl); wrog_3.setFillColor(wr_kl); wrog_4.setFillColor(wr_kl);
+	sf::Color wr_outline(sf::Color::Yellow);//kolor otoczki
+	wrog_1.setOutlineColor(wr_outline); wrog_2.setOutlineColor(wr_outline); wrog_3.setOutlineColor(wr_outline); wrog_4.setOutlineColor(wr_outline);
+	wrog_1.setOutlineThickness(-5.f); wrog_2.setOutlineThickness(-5.f); wrog_3.setOutlineThickness(-5.f); wrog_4.setOutlineThickness(-5.f);
+
+	//PĘTLA GRY
+	while (menu.isOpen())
 	{
 		sf::Event event;
-		while (glowne.pollEvent(event))
+		while (menu.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				glowne.close();
+				menu.close();
 		}
-		glowne.clear();
-		glowne.draw(wrog_4);
-		glowne.display();
+		menu.clear();
+		menu.draw(wrog_4);
+		menu.display();
 	}
 	return 0;
 }
