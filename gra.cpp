ec2c37048452d44396b224b2a21ea21aa8bd0159
Matelusz <you@example.com>
2023-11-12 18:10:25 +0100

    Wprowadzenie osobnych funkcji do inicjalizowania różnych okien

--- gra.cpp
+++ gra.cpp
@@ -1,16 +1,31 @@
 #include"gra.h"
+
+//****************************************************************KONSTRUKTOR I DESTRUKTOR******************************************************************
+Gra::Gra()//konstruktor
+{
+	//wywo³anie metod inicjalizuj¹cych zmienne i okna - kolejnoœæ taka poniewa¿ w metodzie inicjalizujZmienne() ustawi³em wskaŸniki na wartoœæ NULL
+	this->inicjalizujZmienne();
+	this->inicjalizujMenu();//domyœlnie wartoœæ wskaŸnika *okno inicjalizuje okno menu, bo jest pierwsz¹ inicjalizowan¹ funkcj¹
+	void inicjalizujGlowne();
+	void inicjalizujHelp();
+	void inicjalizujEsc();
+}
+Gra::~Gra()//destruktor
+{
+	delete this->okno;//usuwam daynamicznie zaalokowane okna
+}
 //*****************************************************************METODY PUBLICZNE KLASY GRA*****************************************************************
 void Gra::pollEvents()//zwracanie wydarzeñ
 {
-	while (this->menu->pollEvent(this->event))
+	while (this->okno->pollEvent(this->event))
 	{
 		switch (this->event.type)
 		{
 		case sf::Event::Closed:
-			this->menu->close();
+			this->okno->close();
 		case sf::Event::KeyPressed:
 			if (this->event.key.code == sf::Keyboard::Escape)
-				this->menu->close();
+				this->okno->close();
 		}
 	}
 }
@@ -20,53 +35,47 @@ void Gra::aktualizuj()
 }
 void Gra::wyswietlaj()
 {
-	this->menu->clear();
-	this->glowne->clear();
-	this->help->clear();
-	this->esc->clear();
+	this->okno->clear();
 
 	//tu bêdzie rysowane wszystko
 
-	this->menu->display();
-	this->glowne->display();
-	this->help->display();
-	this->esc->display();
+	this->okno->display();
 }
 //*****************************************************************METODY PRYWATNE KLASY GRA*****************************************************************
 void Gra::inicjalizujZmienne()//inicjalizowanie zmiennych prywatnych klasy gra - silnika gry, tak ¿eby mo¿naby³o ich u¿ywaæ
 {
-	this->menu = nullptr;//nullpointer - wska¿niki wskazuj¹ na wartoœæ NULL
-	this->glowne = nullptr;
-	this->help= nullptr;
-	this->esc = nullptr;
+	this->okno = nullptr;//nullpointer - wska¿niki wskazuj¹ na wartoœæ NULL
 }
-void Gra::inicjalizujOkna()
+void Gra::inicjalizujMenu()
 {
-	this->menu=new sf::RenderWindow(sf::VideoMode(600.f, 600.f), "Menu");//dynamicznie zaalokowane okno
-	this->glowne = new sf::RenderWindow (sf::VideoMode(400.f, 400.f), "Projekt-PACMAN");
-	this->help = new sf::RenderWindow (sf::VideoMode(200.f, 200.f), "Pomoc");
-	this->esc = new sf::RenderWindow (sf::VideoMode(200.f, 200.f), "Opuszczanie gry");
+	this->videoMode.height = 600;
+	this->videoMode.width = 600;
+	this->nazwa = "Menu";
+	this->okno = new sf::RenderWindow(this->videoMode, nazwa);
 }
-
+void Gra::inicjalizujGlowne()
+{
+	this->videoMode.height = 400;
+	this->videoMode.width = 400;
+	this->nazwa = "Projekt-PACMAN";
+	this->okno = new sf::RenderWindow(this->videoMode, nazwa);
+}
+void Gra::inicjalizujHelp()
+{
+	this->videoMode.height = 200;
+	this->videoMode.width = 200;
+	this->nazwa = "Pomoc";
+	this->okno = new sf::RenderWindow(this->videoMode, nazwa);
+}
+void Gra::inicjalizujEsc()
+{
+	this->videoMode.height = 200;
+	this->videoMode.width = 200;
+	this->nazwa = "Opuszczanie gry";
+	this->okno = new sf::RenderWindow(this->videoMode, nazwa);
+}
 //*******************************AKCESORY-ma dostep do sk³adowych obiektu, mo¿e je wyœwietlaæ ale nie zmienia ich wartoœci**********************************
 const bool Gra::getWindowIsOpen()const//metoda dzia³aj¹ca jak pêtla while(okno.isOpen)
 {
-	return this->menu->isOpen();//na chwile obecn¹ tak to zostawie----------------------------------------------------------------------------------DO POPRAWY
-	return this->glowne->isOpen();
-	return this->help->isOpen();
-	return this->esc->isOpen();
-}
-//****************************************************************KONSTRUKTOR I DESTRUKTOR******************************************************************
-Gra::Gra()//konstruktor
-{
-	//wywo³anie metod inicjalizuj¹cych zmienne i okna - kolejnoœæ taka poniewa¿ w metodzie inicjalizujZmienne() ustawi³em wskaŸniki na wartoœæ NULL
-	this->inicjalizujZmienne();
-	this->inicjalizujOkna();
-}
-Gra::~Gra()//destruktor
-{
-	delete this->menu;//usuwam daynamicznie zaalokowane okna
-	delete this->glowne;
-	delete this->help;
-	delete this->esc;
+	return this->okno->isOpen();//na chwile obecn¹ tak to zostawie----------------------------------------------------------------------------------DO POPRAWY
 }
--- gra.h
+++ gra.h
@@ -3,19 +3,25 @@
 
 //klasa dzia³aj¹ca jako silnik gry
 class Gra
-{
-
+{	
 	//zmienne wskaŸnikowe odopowiadaj¹ce za okna - wskaŸniki daltego, ¿e bêdê chcia³ dynamicznie alokowaæ te okna - usuwaæ je i tworzyæ wtedy kiedy potrzebuje
-	sf::RenderWindow* menu;
-	sf::RenderWindow* glowne;
+	/*sf::RenderWindow* menu;
+	sf::RenderWindow* glowne;--------------na wszelki tak, ¿eby mieæ jak by³o wczeœniej
 	sf::RenderWindow* help;
-	sf::RenderWindow* esc;
+	sf::RenderWindow* esc;*/
+
+	sf::VideoMode videoMode;
+	sf::RenderWindow *okno;
+	std::string nazwa;
 
 	sf::Event event;//z tego nie robiê zmiennej wskaŸnikowej i nie inicjalizuje go bo ma domyœln¹ inicjalizacjê
 
 	//prywatne metody
 	void inicjalizujZmienne();
-	void inicjalizujOkna();
+	void inicjalizujMenu();
+	void inicjalizujGlowne();
+	void inicjalizujHelp();
+	void inicjalizujEsc();
 public:
 	Gra();//konstruktor
 	~Gra();//destruktor
